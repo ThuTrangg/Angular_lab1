2e5b33fd9133c87e049b01dfbf059e5abe09818a
ThuTrangg <shihtzuu08@gmail.com>
2020-03-17 20:26:12 +0700

    update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { User } from "../User";
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
 export class ProductComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  user: User = {
+    name: "Trang",
+    price: 220,
+    desc: "i love you"
+  };
+  onclick(a) {
+    this.user.desc = !this.user.desc;
   }
-
+  hendKeyup(b) {
+    this.user.name = b.target.value;
+  }
 }
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,24 +1,7 @@
-export const products = [
-  {
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
-  },
-  {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
-  },
-  {
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
-  }
-];
+export class user{
+  name:String;
+  price:number;
+  desc:String;
+}
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
